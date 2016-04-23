b7cd13e83166d764ee41c67fc06933195a759dd1
Ryan Tsao <rtsao@uber.com>
2016-04-23 17:31:42 +0200

    Added value fallback support and automatic vendor prefixing

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var epistyle = require('epistyle/passthrough');
+var prefix = require('inline-style-prefix-all/dist/prefixAll');
 var styletron = require('styletron');
 
 module.exports = stylematic;
 
 function stylematic(styles) {
-  var scoped = epistyle(styles);
+  var prefixed = prefix(styles);
+  var scoped = epistyle(prefixed);
 
   if (scoped.css) {
     styletron.injectOnce(scoped.className, scoped.css);
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var test = require('tape');
+var styletron = require('styletron');
 var stylematic = require('../');
 
 test('passthrough styles', function(t) {
@@ -8,5 +9,15 @@ test('passthrough styles', function(t) {
   t.deepEqual(result.passthrough, {color: 'red'});
   t.equal(result.css, '');
   t.equal(result.className, '');
+  t.end();
+});
+
+test('prefix test', function(t) {
+  styletron.startBuffering();
+  var result = stylematic({width: 'calc(50%)'});
+  var expectedCss = '._style_2LqhMD {\n  width: -webkit-calc(50%) !important;\n  width: -moz-calc(50%) !important;\n  width: calc(50%) !important\n}';
+  t.equal(result.css, expectedCss);
+  t.equal(styletron.flushBuffer(), expectedCss);
+  t.equal(result.className, '_style_2LqhMD');
   t.end();
 });
